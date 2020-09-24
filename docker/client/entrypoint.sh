5aeabe55a224717027cf124e5cf261b637671276
suika <2320837+Suika@users.noreply.github.com>
2020-09-24 02:25:49 +0200

    Patch requests to honour no_proxy
    https://github.com/psf/requests/issues/4871

--- docker/client/entrypoint.sh
+++ docker/client/entrypoint.sh
@@ -5,12 +5,18 @@ GROUP_ID=${GID}
 
 echo "Starting Hydrus with UID/GID : $USER_ID/$GROUP_ID"
 
-echo "Patching Hydrus"
-
 cd /opt/hydrus/
 
-if [ -f "/patch.patch" ]; then
-  patch -f -p1 -i /patch.patch
+if [ -f "/opt/hydrus/docker/client/patch.patch" ]; then
+  echo "Patching Hydrus"
+  patch -f -p1 -i /opt/hydrus/docker/client/patch.patch
+fi
+
+if [ -f "/opt/hydrus/docker/client/requests.patch" ]; then
+  pushd /usr/lib/python3.7/site-packages/requests
+    echo "Patching Requests"
+    patch -f -p1 -i /opt/hydrus/docker/client/requests.patch
+  popd
 fi
 
 #if [ $USER_ID !=  0 ] && [ $GROUP_ID != 0 ]; then
